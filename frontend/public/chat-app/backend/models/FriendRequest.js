445894f968d6181948fc38e5a96f98eb202ec740
mouhyieddine_s <151170493+mohy4002@users.noreply.github.com>
2025-08-13 23:27:42 +0100

    Delete frontend/public directory

--- frontend/public/chat-app/backend/models/FriendRequest.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const FriendRequestSchema = new mongoose.Schema({
-  from: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  to:   { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('FriendRequest', FriendRequestSchema);
